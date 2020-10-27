9acdac12b38ba41de5728900301a0c22c34fe033
xiaxia112 <33377370+xiaxia112@users.noreply.github.com>
2020-10-27 14:36:39 +0800

    create a stateNode to store the state after one Command

---
+++ src/core/StateNode.java
@@ -0,0 +1,31 @@
+package core;
+
+import Commands.Command;
+
+public class StateNode {
+    private String strState;
+    private Command command;
+
+    public StateNode(String strState, Command command) {
+        this.strState = strState;
+        this.command = command;
+    }
+
+    public String getStrState() {
+        return strState;
+    }
+
+    public void setStrState(String strState) {
+        this.strState = strState;
+    }
+
+    public Command getCommand() {
+        return command;
+    }
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+
+}
